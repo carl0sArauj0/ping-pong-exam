292eee62f4a3059c7398662382555429306e1227
carl0sArauj0 <caraujoro@unal.edu.co>
2026-01-12 20:32:56 -0500

    correciones

--- grpc-ping-pong/client/main.go
+++ grpc-ping-pong/client/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	// "server:50051" funciona gracias a la red interna de Docker Compose
+	
 	conn, err := grpc.Dial("server:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("No se pudo conectar: %v", err)
@@ -19,7 +19,7 @@ func main() {
 	defer conn.Close()
 	c := pb.NewPingServiceClient(conn)
 
-	// Intentar enviar Ping cada 2 segundos para dar tiempo al servidor a subir
+	
 	for {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		r, err := c.Ping(ctx, &pb.PingRequest{Message: "Ping"})
